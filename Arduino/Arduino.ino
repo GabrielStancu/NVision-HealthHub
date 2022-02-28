119feee7f6a20191ba5149d97c73f074f5ad2c3e
GabrielStancu <gabriel.stancu07@yahoo.com>
2022-02-28 23:38:43 +0200

    Integrated MAX30100, removed heartbeat sensor

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,8 +1,8 @@
 #include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
 
 #define DEB_INTERVAL_MS 5000
 #define REPORTING_PERIOD_MS 1
-#define HEARTBEAT A0
 #define TEMPERATURE A1
 #define LO_PLUS 10
 #define LO_MINUS 11
@@ -11,29 +11,30 @@
 
 #define DONE_OP_LED 8
 #define NO_OP_LED 7
-#define HB_LED 6
-#define TEMP_LED 5
-#define ECG_LED 4
+#define TEMP_LED 6
+#define ECG_LED 5
+#define MAX_LED 4
 
 #define REQ_HB 5
 #define REQ_TMP 5
 #define REQ_ECG 10
+
+PulseOximeter pox;
 
 bool pushingOpBtn = false;
 int sendMode = 0;
 uint32_t tsLastReport = 0;
 int lastSwitchDetectedMIllis;
 const char separator = ';';
-unsigned long timestamp;
 
 int hbCnt = 0;
 int tmpCnt = 0;
 int ecgCnt = 0;
 
 bool canGoNextState = false;
+unsigned long timestamp;
 
 void setup() { 
-  pinMode(HEARTBEAT, INPUT);
   pinMode(TEMPERATURE, INPUT);
   pinMode(LO_PLUS, INPUT); 
   pinMode(LO_MINUS, INPUT); 
@@ -41,10 +42,11 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(OP_BTN), opBtnRise, RISING);
   pinMode(DONE_OP_LED, OUTPUT);
   pinMode(NO_OP_LED, OUTPUT);
-  pinMode(HB_LED, OUTPUT);
   pinMode(TEMP_LED, OUTPUT);
   pinMode(ECG_LED, OUTPUT);
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
   lastSwitchDetectedMIllis = millis();
+  timestamp = millis();
   Serial.begin(9600);
 }
 
@@ -69,13 +71,14 @@ void sendData() {
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
     switch(sendMode) {
       case 1:
-        sendHeartbeat();
-        break;
-      case 2:
         sendTemperature();
         break;
-      case 3:
+      case 2:
         sendEcg();
+        break;
+      case 3: 
+        sendPulse();
+        sendOxygen();
         break;
       default: 
         break;
@@ -83,18 +86,6 @@ void sendData() {
 
     tsLastReport = millis();
   }
-}
-
-float measureHeartbeat() {
-  float pulse;
-  float correctiveFactor = 1.5;
-  int sum = 0;
-  for (int i = 0; i < 20; i++)
-    sum += analogRead(HEARTBEAT);
-  pulse = sum / 20.00 * correctiveFactor;
-  timestamp = millis();
-
-  return pulse;
 }
 
 float measureTemperature() {
@@ -111,27 +102,21 @@ float measureEcg() {
   if((digitalRead(LO_PLUS) == HIGH)||(digitalRead(LO_MINUS) == HIGH)){
     return 0;
   }
-
+  float ecg = analogRead(ECG);
   timestamp = millis();
-  return analogRead(ECG);
+  return ecg;
 }
 
-void sendHeartbeat() {
-  float heartbeat = measureHeartbeat();
-  String heartbeatStr = String(heartbeat, 3);
-  String timestampStr = String(timestamp);
-  String sendValue = "";
-  const float minValidValue = 0;
-  const float maxValidValue = 250;
-  if (heartbeat >= minValidValue && heartbeat <= maxValidValue) {
-    countHeartbeat();
-    sendValue.concat("HB");
-    sendValue.concat(separator);
-    sendValue.concat(heartbeatStr);
-    sendValue.concat(separator);
-    sendValue.concat(timestampStr);
-    Serial.println(sendValue);
-  }
+float measurePulse() {
+  float pulse = pox.getHeartRate();
+  timestamp = millis();
+  return pulse;
+}
+
+float measureSpO2() {
+  float spO2 = pox.getSpO2();
+  timestamp = millis();
+  return spO2;
 }
 
 void sendTemperature() {
@@ -170,14 +155,39 @@ void sendEcg() {
   }
 }
 
-void countHeartbeat() {
-  hbCnt++;
-  if(hbCnt >= REQ_HB) {
-    digitalWrite(DONE_OP_LED, HIGH);
-    canGoNextState = true;
-  } 
-  else {
-    canGoNextState = false;
+void sendPulse() {
+  float pulse = measurePulse();
+  String pulseStr = String(pulse, 3);
+  String timestampStr = String(timestamp);
+  String sendValue = "";
+  const int minValidValue = 30;
+  const int maxValidValue = 200;
+  if (pulse >= minValidValue && pulse <= maxValidValue) {
+    countEcg();
+    sendValue.concat("BPM");
+    sendValue.concat(separator);
+    sendValue.concat(pulseStr);
+    sendValue.concat(separator);
+    sendValue.concat(timestampStr);
+    Serial.println(sendValue);
+  }
+}
+
+void sendOxygen() {
+  float oxygen = measureSpO2();
+  String oxygenStr = String(oxygen, 3);
+  String timestampStr = String(timestamp);
+  String sendValue = "";
+  const int minValidValue = -1;
+  const int maxValidValue = 101;
+  if (oxygen >= minValidValue && oxygen <= maxValidValue) {
+    countEcg();
+    sendValue.concat("OXY");
+    sendValue.concat(separator);
+    sendValue.concat(oxygenStr);
+    sendValue.concat(separator);
+    sendValue.concat(timestampStr);
+    Serial.println(sendValue);
   }
 }
 
@@ -213,22 +223,22 @@ void resetState() {
 
 void lightOpLeds() {
   digitalWrite(NO_OP_LED, LOW);
-  digitalWrite(HB_LED, LOW);
   digitalWrite(TEMP_LED, LOW);
   digitalWrite(ECG_LED, LOW);
+  digitalWrite(MAX_LED, LOW);
 
   switch(sendMode) {
     case 0:
       digitalWrite(NO_OP_LED, HIGH);
       break;
     case 1: 
-      digitalWrite(HB_LED, HIGH);
-      break;
-    case 2: 
       digitalWrite(TEMP_LED, HIGH);
       break;
-    case 3: 
+    case 2: 
       digitalWrite(ECG_LED, HIGH);
+      break;
+    case 3: 
+      digitalWrite(MAX_LED, HIGH);
       break;
     default: 
       sendMode = 0;
